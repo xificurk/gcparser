4fad6649c7a8fc7657a871ccc3092f442620af1d
Petr Morávek [Xificurk] <xificurk@gmail.com>
2010-01-30 22:14:25 +0100

    Example script: update comments.

--- example.py
+++ example.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Import GCparser
+""" Import GCparser
+"""
 from gcparser import GCparser
 
-# Create GCparser class, you can also pass the third dataDir argument, then
-#   GCparser will use that directory to store login cookies, and reuse them,
-#   if possible.
+""" Create GCparser class, you can also pass the third dataDir argument, then
+    GCparser will use that directory to store login cookies, and reuse them,
+    if possible.
+"""
 gcp = GCparser("username", "secret")
 
-# Let's take a look at your finds
+""" Let's take a look at your finds
+"""
 myfinds = gcp.parse("myFinds").getList()
 print("MyFinds:")
 print(myfinds)
 
 print()
 
-# And now, parse details about some cache, you can also specify waypoint
-#   parameter instead of guid.
+""" And now, parse details about some cache, you can also specify waypoint
+    parameter instead of guid.
+"""
 details = gcp.parse("cache", guid="ed5b20b7-fdca-4e59-b518-3412154d49d0").getDetails()
 print("Cache:")
 print(details)
 
-# Let's update our profile
+""" Let's update our profile.
+"""
 gcp.parse("editProfile", "Pyggs profile update test.").save()
