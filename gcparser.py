8e63bb7b80be8f69042e8e86d08a343b39fc6715
Petr Morávek [Xificurk] <xificurk@gmail.com>
2010-09-02 16:55:14 +0200

    [gc.com update] fix MyFindsParser

--- gcparser.py
+++ gcparser.py
@@ -20,7 +20,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-__version__ = "0.4.8"
+__version__ = "0.4.9"
 __all__ = ["GCparser", "Fetcher", "BaseParser", "CacheParser", "MyFindsParser", "SeekParser", "EditProfile", "CredentialsException", "LoginException"]
 
 
@@ -698,10 +698,10 @@ class CacheParser(BaseParser):
 __pcresMask["logsFound"] = ("\s*<img[^>]*(Found it|Webcam Photo Taken|Attended)[^>]*>", re.I)
 # 7/23/2008
 __pcresMask["logsDate"] = ("\s*([0-9]+)/([0-9]+)/([0-9]+)", re.I)
-# <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=331f0c62-ef78-4ab3-b8d7-be569246771d" class="lnk"><img src="http://www.geocaching.com/images/wpttypes/sm/2.gif" title="Traditional Cache" width="16" height="16" border="0"/> <span>Stepankovi hrosi</span></a>&nbsp;
-# <strike><a href="http://www.geocaching.com/seek/cache_details.aspx?guid=d3e80a41-4218-4136-bb63-ac0de3ef0b5a" class="lnk"><img src="http://www.geocaching.com/images/wpttypes/sm/8.gif" title="Unknown Cache" width="16" height="16" border="0"/> <span><strike>Barva Kouzel</strike></span></a></strike>&nbsp;
-# <span class="Strike Warning"><a href="http://www.geocaching.com/seek/cache_details.aspx?guid=29444383-4607-4e2d-bc65-bcf2e9919e5d" class="lnk"><img src="http://www.geocaching.com/images/wpttypes/sm/2.gif" title="Traditional Cache" width="16" height="16" border="0"/> <span><strike><font color="red">Krizovatka na kopci / Crossroad on a hill</font></strike></span></a></span>&nbsp;
-__pcresMask["logsName"] = ("\s*(<span class=\"Strike Warning\">)?(<strike>)?<a href=['\"][^'\"]*/seek/cache_details.aspx\?guid=([a-z0-9-]+)['\"][^>]*><img[^>]*>\s*<span>(<strike>)?(<font color=\"red\">)?([^<]+)(</font>)?(</strike>)?</span></a>", re.I)
+# <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=331f0c62-ef78-4ab3-b8d7-be569246771d" class="ImageLink"><img src="http://www.geocaching.com/images/wpttypes/sm/2.gif" title="Traditional Cache" /></a> <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=331f0c62-ef78-4ab3-b8d7-be569246771d">Stepankovi hrosi</a>&nbsp;
+# <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=d3e80a41-4218-4136-bb63-ac0de3ef0b5a" class="ImageLink"><img src="http://www.geocaching.com/images/wpttypes/sm/8.gif" title="Unknown Cache" /></a> <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=d3e80a41-4218-4136-bb63-ac0de3ef0b5a"><span class="Strike">Barva Kouzel</span></a>&nbsp;
+# <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=29444383-4607-4e2d-bc65-bcf2e9919e5d" class="ImageLink"><img src="http://www.geocaching.com/images/wpttypes/sm/2.gif" title="Traditional Cache" /></a> <a href="http://www.geocaching.com/seek/cache_details.aspx?guid=29444383-4607-4e2d-bc65-bcf2e9919e5d"><span class="Strike OldWarning">Krizovatka na kopci / Crossroad on a hill</span></a>&nbsp;
+__pcresMask["logsName"] = ("\s*<a[^>]*>\s*<img[^>]*>\s*</a>\s*<a href=['\"][^'\"]*/seek/cache_details.aspx\?guid=([a-z0-9-]+)['\"][^>]*>\s*(<span class=['\"]Strike(\s*OldWarning)?['\"]>)?\s*([^<]+)\s*(</span>)?\s*</a>", re.I)
 # <a href="http://www.geocaching.com/seek/log.aspx?LUID=a3e234b3-7d34-4a26-bde5-487e4297133c" target="_blank" title="Visit Log">Visit Log</a>
 __pcresMask["logsLog"] = ("\s*<a href=['\"][^'\"]*/seek/log.aspx\?LUID=([a-z0-9-]+)['\"][^>]*>Visit Log</a>", re.I)
 
@@ -749,17 +749,14 @@ class MyFindsParser(BaseParser):
                     if "guid" not in cache:
                         match = pcre("logsName").match(line)
                         if match is not None:
-                            cache["guid"] = match.group(3)
-                            cache["name"] = unescape(match.group(6)).strip()
-                            if match.group(1):
-                                cache["archived"] = 1
+                            cache["guid"] = match.group(1)
+                            cache["name"] = unescape(match.group(4)).strip()
+                            cache["disabled"] = 0
+                            cache["archived"] = 0
+                            if match.group(2):
                                 cache["disabled"] = 1
-                            else:
-                                cache["archived"] = 0
-                                if match.group(2):
-                                    cache["disabled"] = 1
-                                else:
-                                    cache["disabled"] = 0
+                                if match.group(3):
+                                    cache["archived"] = 1
                             self.log.log(LOG_PARSER, "guid = {0}".format(cache["guid"]))
                             self.log.log(LOG_PARSER, "name = {0}".format(cache["name"]))
                             self.log.log(LOG_PARSER, "disabled = {0}".format(cache["disabled"]))
