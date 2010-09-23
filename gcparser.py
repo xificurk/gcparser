9e922dcd5bac2634dc5ca346cf4ab892dda618e7
Petr Morávek [Xificurk] <xificurk@gmail.com>
2010-09-23 21:45:40 +0200

    v0.5.0

--- gcparser.py
+++ gcparser.py
@@ -20,7 +20,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-__version__ = "0.4.9"
+__version__ = "0.5.0"
 __all__ = ["GCparser", "Fetcher", "BaseParser", "CacheParser", "MyFindsParser", "SeekParser", "EditProfile", "CredentialsException", "LoginException"]
 
 
